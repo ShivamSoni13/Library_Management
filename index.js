cddf562af2bab5f50cd1369cf50cb630ff85f814
1234bhaskar <bhaskar2021chand@gmail.com>
2023-12-17 19:23:37 +0530

    mongoose.connect() updated and worked

--- index.js
+++ index.js
@@ -2,30 +2,21 @@ const express = require("express");
 const app = express();
 const Port = 5000;
 const userRoute = require("./routes/user");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require("mongoose");
 // import { userRoute } from "./routes/user";
 app.use(express.json());
 app.use("/api", userRoute);
-const uri = "mongodb+srv://libmgmt:12345@libmgmt.cynijla.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-async function run() {
-  try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Connected to Database Succesfully");
-  } finally {
-    await client.close();
-  }
-}
-run().catch(console.dir);
+const uri =
+  "mongodb+srv://libmgmt:12345@libmgmt.cynijla.mongodb.net/?retryWrites=true&w=majority";
 
-
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("database is connected successfully on -> " + uri);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 app.listen(Port, () => {
   console.log("server is listening at port " + Port);
