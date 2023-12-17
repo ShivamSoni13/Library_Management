738e7ecdb060dab72bc5aa82e757afe90f5ac07d
1234bhaskar <bhaskar2021chand@gmail.com>
2023-12-17 19:22:06 +0530

    userId remv from(19),up->(/:userId)

--- routes/user.js
+++ routes/user.js
@@ -16,7 +16,6 @@ router.post("/register", async (req, res) => {
       });
     } else {
       const user = await User.create({
-        userID,
         username,
         email,
         age,
@@ -33,29 +32,28 @@ router.post("/register", async (req, res) => {
   }
 });
 // Define the route to get all registered users
-router.get('/User', async (req, res) => {
+router.get("/User", async (req, res) => {
   try {
     const users = await User.find();
     res.json(users);
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 // Define the route to get a single registered user by ID
-router.get('/User/:userId', async (req, res) => {
+router.get("/User/:userId", async (req, res) => {
   const userId = req.params.userId;
 
   try {
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
-    }
-    else()=>{
-    res.json(user);
+      return res.status(404).json({ error: "User not found" });
+    } else {
+      res.json(user);
     }
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
