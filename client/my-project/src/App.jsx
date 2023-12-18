59be75c78a1a766345901750897f10edded655a9
1234bhaskar <bhaskar2021chand@gmail.com>
2023-12-18 16:07:59 +0530

    added Home and RegistereUser

--- client/my-project/src/App.jsx
+++ client/my-project/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Login from './pages/Login'
 import { BrowserRouter as Router  ,Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import RegisterUser from './pages/RegisterUser'
 
 const App = () => {
   return (
     <div className=''>
       <Router>
       <Routes>
+        <Route element={<Home/>} path='/'/>
         <Route element={<Login/>} path='/adminLogin'/>
+        <Route element={<RegisterUser/>} path='/registerUser'/>
+        
       </Routes>
       </Router>
     </div>
