71ab0434ff760d1e5746dd29e921ca3f554a4e54
1234bhaskar <bhaskar2021chand@gmail.com>
2023-12-18 16:11:33 +0530

    Added Home Page

---
+++ client/my-project/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from '../components/Footer'
+
+function Home() {
+  return (
+    <div>
+        <div >
+            <header className='text-center text-6xl'>Library</header>
+            <footer className=''><Footer/></footer>
+        </div>
+    </div>
+  )
+}
+
+export default Home
