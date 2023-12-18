c6aef33bb64800961542cb4bbf2a0a2de50f2090
Shivam Kumar <satishshivam697@gmail.com>
2023-12-18 13:36:45 +0530

    Delete user and Update Fee status

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../model/User.js");
 
-//post Request for registering new user
+// Post Request for registering a new user
 router.post("/register", async (req, res) => {
   try {
     const { username, email, age, address, phone, createdAt, feeStatus } =
@@ -11,11 +11,11 @@ router.post("/register", async (req, res) => {
     const user = await User.findOne({ email });
 
     if (user) {
-      res.json({
+      res.status(400).json({
         error: "User already registered",
       });
     } else {
-      const user = await User.create({
+      const newUser = await User.create({
         username,
         email,
         age,
@@ -24,15 +24,16 @@ router.post("/register", async (req, res) => {
         createdAt,
         feeStatus,
       });
-      res.json({ user }).status(200);
+      res.status(201).json({ user: newUser });
     }
   } catch (e) {
-    console.log(e);
-    res.status(500).json("User not Registered");
+    console.error(e);
+    res.status(500).json({ error: "User not registered" });
   }
 });
+
 // Define the route to get all registered users
-router.get("/User", async (req, res) => {
+router.get("/user", async (req, res) => {
   try {
     const users = await User.find();
     res.json(users);
@@ -42,7 +43,7 @@ router.get("/User", async (req, res) => {
 });
 
 // Define the route to get a single registered user by ID
-router.get("/User/:userId", async (req, res) => {
+router.get("/user/:userId", async (req, res) => {
   const userId = req.params.userId;
 
   try {
@@ -57,5 +58,48 @@ router.get("/User/:userId", async (req, res) => {
   }
 });
 
-//export { router as userRoute };
+// Check fee Status and Update it
+router.put('/update-fee-status/:userId', async (req, res) => {
+  const { userId } = req.params;
+  const { feeStatus } = req.body;
+
+  try {
+    // Check if the user exists
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Update fee status
+    user.feeStatus = feeStatus;
+    await user.save();
+
+    return res.status(200).json({ message: 'Fee status updated successfully', user });
+  } catch (error) {
+    console.error('Error updating fee status:', error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+// Delete user endpoint
+router.delete('/delete-user/:userId', async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    // Check if the user exists
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Delete the user
+    await user.remove();
+
+    return res.status(200).json({ message: 'User deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
 module.exports = router;
