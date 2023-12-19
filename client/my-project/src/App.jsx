4e33f93f4433257af78ebfb6076949ec7992b8ad
1234bhaskar <bhaskar2021chand@gmail.com>
2023-12-19 20:38:25 +0530

    added customer,identity route

--- client/my-project/src/App.jsx
+++ client/my-project/src/App.jsx
@@ -3,16 +3,23 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import RegisterUser from './pages/RegisterUser';
+import Customers from './pages/Customers';
+import { UserContextProvider } from './context/UserContext';
+import IdentificationPage from './pages/IdentificationPage';
 
 const App = () => {
   return (
     <div>
       <Router>
+      <UserContextProvider>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<RegisterUser />} />
+          <Route path="/customersinfo" element={<Customers/>} />
+          <Route path="/identity/:customerId" element={<IdentificationPage/>}/>
         </Routes>
+      </UserContextProvider>
       </Router>
     </div>
   );
