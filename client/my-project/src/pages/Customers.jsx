d5459045a11727f499dc533632996e44eac5c049
1234bhaskar <bhaskar2021chand@gmail.com>
2023-12-19 20:42:20 +0530

    Created Customers Page

---
+++ client/my-project/src/pages/Customers.jsx
@@ -0,0 +1,43 @@
+import React, { useContext, useEffect, useState } from 'react'
+import SearchCustomer from '../components/SearchCustomer'
+import Customer from '../components/Customer'
+import axios from 'axios';
+import { userRequest } from '../util/requestMethod';
+import { UserContext } from '../context/UserContext';
+
+
+function Customers() {
+
+    //const[customers,setCustomers]=useState([]);
+    const {customers,setCustomers}=useContext(UserContext);
+    useEffect(()=>{
+        const fetchData= async ()=>{
+            const response= await userRequest.get("/user").then((prop)=>{
+              setCustomers(prop.data);
+              //console.log(customers);
+            }).catch((e)=>{
+              console.log(e);
+            })
+        }
+        fetchData();
+    },[])
+ 
+  return (
+    <div>
+        <div>
+            <SearchCustomer/>
+        </div>
+            <header className='text-3xl text-center'>Customers</header>
+        <div>
+            <Customer key={customers._id}
+             name={customers.username}
+             id={customers._id}
+             address={customers.address}
+             phone={customers.phone}
+              />
+        </div>
+    </div>
+  )
+}
+
+export default Customers
