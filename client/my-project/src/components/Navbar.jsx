6c471427a9a2c3a9a2f15cea53f88c4e3ffb0e40
1234bhaskar <bhaskar2021chand@gmail.com>
2023-12-20 17:05:08 +0530

    created navbar

--- client/my-project/src/components/Navbar.jsx
+++ client/my-project/src/components/Navbar.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom';
 
 function Navbar() {
+   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-        
+       <nav className="flex items-center justify-between flex-wrap px-2 sm:p-3 py-2  border">
+     {/* <div className="flex items-center flex-shrink-0 text-white mr-6 lg:mr-72">
+       {/* <img src={locofy} className="w-100 h-10 mr-2" alt="Logo" /> 
+     </div>  */}
+    
+     <div className="block lg:hidden">
+       <button
+         onClick={() => setIsOpen(!isOpen)}
+         className="flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400"
+       >
+         <svg
+           className={`fill-current h-4 w-4 ${isOpen ? "hidden" : "block"}`}
+           viewBox="0 0 20 20"
+           xmlns="http://www.w3.org/2000/svg"
+         >
+           <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+         </svg>
+         <svg
+           className={`fill-current h-4 w-4 ${isOpen ? "block" : "hidden"}`}
+           viewBox="0 0 20 20"
+           xmlns="http://www.w3.org/2000/svg"
+         >
+           <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+         </svg>
+       </button>
+     </div>
+     <div
+       className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${isOpen ? "block" : "hidden"}`}
+     >
+       <div className="text-sm lg:flex-grow my-4 sm:my-0 sm:flex sm:justify-start " >
+         <NavLink to={'/customersinfo'}
+         style={({ isActive }) => ({
+                                backgroundColor: isActive
+                                    && "#200E3A",
+                                    color: isActive && "white",
+                                    fontStyle: isActive && "bold",      
+                            })}
+          className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4 sm:border p-2 sm:p-3 sm:bg-gray-400 rounded-md w-fit">
+           View Customers
+         </NavLink>
+         <NavLink to={'/register'}
+          style={({ isActive }) => ({
+                                backgroundColor: isActive
+                                    && "#200E3A",
+                                    color: isActive && "white",
+                                    fontStyle: isActive && "bold",      
+                            })}
+         className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4 sm:border p-2 sm:p-3 sm:bg-gray-400 rounded-md w-fit">
+           Register User
+         </NavLink>
+         {/* <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4">
+           Third Link
+         </a>
+         <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4">
+           Fourth Link
+         </a> */}
+       </div>
+       <div>
+         <button className="inline-flex items-center bg-red-500 border-0 py-2 px-4 text-white rounded-md">
+           Logout
+         </button>
+       </div>
+     </div>
+   </nav>          
     </div>
   )
 }
