f6cd767f98333b23903a018e3164ba7c9b8648e7
1234bhaskar <bhaskar2021chand@gmail.com>
2023-12-20 15:14:58 +0530

    resolved registered user

--- client/my-project/src/pages/RegisterUser.jsx
+++ client/my-project/src/pages/RegisterUser.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { userRequest } from '../util/requestMethod';
 
 const RegisterUser = () => {
   const formFields = [
@@ -32,25 +33,35 @@ const RegisterUser = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('/register', formData);
-
-      if (response.status === 201) {
+       await userRequest.post('/register',formData).then(()=>{
         toast.success('User Registered Successfully');
-        navigate('/IdentificationPage');
-        setFormData(formFields.reduce((acc, field) => {
-          acc[field.name] = '';
-          return acc;
-        }, {}));
-      } else {
-        toast.error('Error Registering User');
-      }
+              navigate('/customersinfo');
+              setFormData(formFields.reduce((acc, field) => {
+                  acc[field.name] = '';
+                  return acc;
+                }, {}));
+              }).catch(()=>{
+                toast.error('Error Registering User');
+                console.log('Error Registering User');
+              })
+
+      // if (response.status === 201) {
+      //   toast.success('User Registered Successfully');
+      //   navigate('/customersinfo');
+      //   setFormData(formFields.reduce((acc, field) => {
+      //     acc[field.name] = '';
+      //     return acc;
+      //   }, {}));
+      // } else {
+      //   toast.error('Error Registering User');
+      // }
     } catch (error) {
-      console.error('Error Registering User:', error);
+      //console.error('Error Registering User:', error);
   
       // Log more details about the error
       console.log('Error Details:', error.response);
   
-      toast.error('Error Registering User');
+      //toast.error('Error Registering User');
     }
   };
 
@@ -59,7 +70,7 @@ const RegisterUser = () => {
       <header className='text-center text-3xl sm:text-6xl'>Register a New Customer</header>
 
       <form
-        onSubmit={handleSubmit}
+        
         className='border-2 border-yellow-300 px-3 sm:px-2 bg-white flex flex-col sm:w-1/3 w-full mx-auto sm:mt-10 py-5 sm:py-10 sm:rounded-md z-10'
       >
         {formFields.map((field) => (
@@ -82,7 +93,7 @@ const RegisterUser = () => {
         ))}
 
         <div className='flex justify-center py-2'>
-          <button className='border bg-yellow-300 text-white p-2 w-1/3 rounded-md sm:w-28' type='submit'>
+          <button className='border bg-yellow-300 text-white p-2 w-1/3 rounded-md sm:w-28' type='submit' onClick={handleSubmit}>
             Register
           </button>
         </div>
