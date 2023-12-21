cde853bd38053e4bc7805ef753a20f05c8bd1016
1234bhaskar <bhaskar2021chand@gmail.com>
2023-12-21 17:21:39 +0530

    removed handleLogout

--- client/my-project/src/components/Navbar.jsx
+++ client/my-project/src/components/Navbar.jsx
@@ -83,7 +83,7 @@ function Navbar() {
        </div>
        
        <div>
-         <button onClick={handleLogout} className="inline-flex items-center bg-red-500 border-0 py-2 px-4 text-white rounded-md">
+         <button  className="inline-flex items-center bg-red-500 border-0 py-2 px-4 text-white rounded-md">
            Logout
          </button>
        </div>
