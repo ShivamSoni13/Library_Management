6faea9968413fe908450e47f52e34089d2444f53
Shivam Kumar <87065373+ShivamSoni13@users.noreply.github.com>
2023-12-26 11:55:25 +0530

    Update Home.jsx

--- client/my-project/src/pages/Home.jsx
+++ client/my-project/src/pages/Home.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
-import Footer from '../components/Footer'
+import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link for navigation
+import Footer from '../components/Footer';
 
 function Home() {
   return (
     <div>
-        <div >
-            <header className='text-center text-6xl'>Library</header>
-            <footer className=''><Footer/></footer>
-        </div>
+      <div>
+        {/* Header */}
+        <header className='flex justify-between items-center p-4 bg-blue-500 text-white'>
+          <h1 className='text-6xl'>Library</h1>
+          {/* Login Button */}
+          <Link to="/login" className='text-xl hover:underline'>
+            Login
+          </Link>
+        </header>
+        
+        {/* Footer */}
+        <footer>
+          <Footer />
+        </footer>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
+
