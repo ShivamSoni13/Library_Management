5015f55b087c1f539ec46014b47490a458d99f56
Shivam Kumar <satishshivam697@gmail.com>
2023-12-27 18:01:24 +0530

    Register User and Update Details Updated

--- client/my-project/src/pages/RegisterUser.jsx
+++ client/my-project/src/pages/RegisterUser.jsx
@@ -24,90 +24,120 @@ const RegisterUser = () => {
   );
 
   const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
+    const { name, value } = e.target;
+
+    switch (name) {
+      case 'username':
+      case 'father':
+        if (/^[a-zA-Z\s]*$/.test(value) || value === '') {
+          setFormData({
+            ...formData,
+            [name]: value,
+          });
+        }
+        break;
+        case 'email':
+          // Updated email validation regular expression
+          if (/^[a-zA-Z0-9._]+@[a-z]+\.[a-z]{2-6}$/.test(value) || value === '') {
+            setFormData({
+              ...formData,
+              [name]: value,
+            });
+          }
+          break;
+        case 'age':
+          if (/^\d*$/.test(value) || value === '') {
+            setFormData({
+              ...formData,
+              [name]: value,
+            });
+          }
+          break;
+        case 'phone':
+          // Allow only numeric values for Phone Number
+          if (/^\d*$/.test(value)) {
+            setFormData({
+              ...formData,
+              [name]: value,
+            });
+          }
+          break;
+        default:
+          setFormData({
+            ...formData,
+            [name]: value,
+          });
+      }
+    };
 
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-       await userRequest.post('/register',formData).then(()=>{
-        toast.success('User Registered Successfully');
-              navigate('/customersinfo');
-              setFormData(formFields.reduce((acc, field) => {
-                  acc[field.name] = '';
-                  return acc;
-                }, {}));
-              }).catch(()=>{
-                toast.error('Error Registering User');
-                console.log('Error Registering User');
-              })
 
-      // if (response.status === 201) {
-      //   toast.success('User Registered Successfully');
-      //   navigate('/customersinfo');
-      //   setFormData(formFields.reduce((acc, field) => {
-      //     acc[field.name] = '';
-      //     return acc;
-      //   }, {}));
-      // } else {
-      //   toast.error('Error Registering User');
-      // }
+    if (!formData.username.trim() || !formData.father.trim() || !formData.email.trim() || !formData.age.trim() || !formData.phone.trim()) {
+      toast.error('All fields are required');
+      return;
+    }
+
+    try {
+      await userRequest.post('/register', formData).then(() => {
+        toast.success('User Registered Successfully');
+        navigate('/customersinfo');
+        setFormData(
+          formFields.reduce((acc, field) => {
+            acc[field.name] = '';
+            return acc;
+          }, {})
+        );
+      }).catch(() => {
+        toast.error('Error Registering User');
+        console.log('Error Registering User');
+      });
     } catch (error) {
-      //console.error('Error Registering User:', error);
-  
-      // Log more details about the error
       console.log('Error Details:', error.response);
-  
-      //toast.error('Error Registering User');
     }
   };
 
   return (
     <div className='bg-khaki'>
       <div>
-        <Navbar/>
+        <Navbar />
       </div>
 
-       <div className='  sm:h-screen  sm:flex sm:justify-center sm:items-center'>
-
-      <form
-        className=' bg-slate-50 flex flex-col sm:w-3/5 w-full mx-auto sm:mt-10 sm:drop-shadow-lg sm:blur-none shadow-black  sm:rounded-md z-10 sm:h-3/4 '
-      >
-      <header className='text-center text-3xl sm:text-5xl py-3 bg-navy text-orange-600 sm:rounded-t-md'>Register a New Customer</header>
-        <div className='sm:my-3 sm:px-10 px-5'>
-          {formFields.map((field) => (
-          <div key={field.name} className='sm:border-none my-2 flex flex-col sm:flex sm:flex-row '>
-            <label
-              htmlFor={field.name}
-              className='sm:flex w-1/5 sm:items-center sm:justify-start text-xs sm:text-lg'
-            >
-              {field.label}
-            </label>
-            <input
-              type={field.type}
-              name={field.name}
-              className='border sm:w-full p-1 rounded-md sm:rounded-sm'
-              placeholder={field.placeholder}
-              value={formData[field.name]}
-              onChange={handleChange}
-            />
+      <div className='  sm:h-screen  sm:flex sm:justify-center sm:items-center'>
+        <form
+          className=' bg-slate-50 flex flex-col sm:w-3/5 w-full mx-auto sm:mt-10 sm:drop-shadow-lg sm:blur-none shadow-black  sm:rounded-md z-10 sm:h-3/4 '
+        >
+          <header className='text-center text-3xl sm:text-5xl py-3 bg-navy text-orange-600 sm:rounded-t-md'>Register a New Customer</header>
+          <div className='sm:my-3 sm:px-10 px-5'>
+            {formFields.map((field) => (
+              <div key={field.name} className='sm:border-none my-2 flex flex-col sm:flex sm:flex-row '>
+                <label
+                  htmlFor={field.name}
+                  className='sm:flex w-1/5 sm:items-center sm:justify-start text-xs sm:text-lg'
+                >
+                  {field.label}
+                </label>
+                <input
+                  type={field.type}
+                  name={field.name}
+                  className='border sm:w-full p-1 rounded-md sm:rounded-sm'
+                  placeholder={field.placeholder}
+                  value={formData[field.name]}
+                  onChange={handleChange}
+                />
+              </div>
+            ))}
           </div>
-        ))}
-        </div>
 
-        <div className='flex justify-center py-2  sm:mt-0'>
-          <button className='border bg-navy text-orange-600 p-2 w-1/3 rounded-md sm:w-28' type='submit' onClick={handleSubmit}>
-            Register
-          </button>
-        </div>
-      </form>
-             </div>
-
+          <div className='flex justify-center py-2  sm:mt-0'>
+            <button className='border bg-navy text-orange-600 p-2 w-1/3 rounded-md sm:w-28' type='submit' onClick={handleSubmit}>
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
 
       <ToastContainer position="bottom-right" autoClose={3000} />
     </div>
--- client/my-project/src/pages/UpdateDetails.jsx
+++ client/my-project/src/pages/UpdateDetails.jsx
@@ -12,7 +12,32 @@ function UpdateDetails({ onUpdate }) {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+
+    switch (name) {
+      case 'username':
+      case 'father':
+        if (/^[a-zA-Z\s]*$/.test(value) || value === '') {
+          setFormData({ ...formData, [name]: value });
+        }
+        break;
+      case 'email':
+        if (/^[a-zA-Z0-9._]+@[a-z]+\.[a-z]{2,6}$/.test(value) || value === '') {
+          setFormData({ ...formData, [name]: value });
+        }
+        break;
+      case 'age':
+        if (/^\d*$/.test(value) || value === '') {
+          setFormData({ ...formData, [name]: value });
+        }
+        break;
+      case 'phone':
+        if (/^\d{0,10}$/.test(value) || value === '') {
+          setFormData({ ...formData, [name]: value });
+        }
+        break;
+      default:
+        setFormData({ ...formData, [name]: value });
+    }
   };
 
   const handleSubmit = (e) => {
@@ -34,6 +59,7 @@ function UpdateDetails({ onUpdate }) {
             className="w-full border rounded-md p-2 focus:outline-none focus:border-blue-400"
           />
         </div>
+
         <div className="mb-4">
           <label className="block text-sm font-medium text-gray-600">Father:</label>
           <input
