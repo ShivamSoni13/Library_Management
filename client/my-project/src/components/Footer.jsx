d129b58f899490b4612f80517fff18a0a59bd55d
1234bhaskar <bhaskar2021chand@gmail.com>
2023-12-28 17:28:01 +0530

    Frontend updated

--- client/my-project/src/components/Footer.jsx
+++ client/my-project/src/components/Footer.jsx
@@ -2,34 +2,39 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
-        {/* <div className='h-30 border w-full'>
-             div*3
-        </div> */}
+    <div className='h-full' >
+<footer className="bg-white  shadow  dark:bg-gray-800 flex flex-col sm:flex-row text-orange-600 items-center justify-start">
+    <ul className="flex flex-col flex-1 sm:pl-10 mt-3 sm:text-xl font-medium  sm:mt-0">
+        <header className='text-orange-600 text-4xl sm:text-7xl font-bold'>Daksh library</header>
+        <li>
+            <span className="hover:underline me-4 md:me-6 ">Owner</span>
+                <span>-</span>
+        </li>
+        <li>
+            <span className="hover:underline me-4 md:me-6">Email</span>
+            <span>-</span>
+        </li>
+        <li>
+            <span  className="hover:underline me-4 md:me-6">Phone no.</span>
+            <span>-</span>
+        </li>
+        <li>
+            <span  className="hover:underline me-4 md:me-6">Address</span>
+            <span>-</span>
+        </li>
         
-
-<footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800 h-40">
-    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
-    </span>
-    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-        <li>
-            <a href="#" class="hover:underline me-4 md:me-6">About</a>
-        </li>
-        <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-        </li>
-        <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-        </li>
-        <li>
-            <a href="#" class="hover:underline">Contact</a>
-        </li>
     </ul>
-    </div>
-</footer>
-
-    </div>
+{/* Maps location */}
+          <div className='  flex-1  hidden sm:flex sm:justify-center'>
+            {/* <iframe src={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3496.1587376424363!2d76.1345054!3d28.804343099999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39126769af8ce3b9%3A0x8d86043f75178844!2sDaksh%20library%20Bhiwani!5e0!3m2!1sen!2sin!4v1703757285150!5m2!1sen!2sin"} width={"100vw"}height={"100vh"}  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d27969.269927980906!2d76.13450500000002!3d28.804343!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39126769af8ce3b9%3A0x8d86043f75178844!2sDaksh%20library%20Bhiwani!5e0!3m2!1sen!2sin!4v1703758989086!5m2!1sen!2sin" width="600"  height="400" style={{ border: "0" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+          <div className=' w-full flex sm:hidden '>
+            {/* <iframe src={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3496.1587376424363!2d76.1345054!3d28.804343099999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39126769af8ce3b9%3A0x8d86043f75178844!2sDaksh%20library%20Bhiwani!5e0!3m2!1sen!2sin!4v1703757285150!5m2!1sen!2sin"} width={"100vw"}height={"100vh"}  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d27969.269927980906!2d76.13450500000002!3d28.804343!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39126769af8ce3b9%3A0x8d86043f75178844!2sDaksh%20library%20Bhiwani!5e0!3m2!1sen!2sin!4v1703758989086!5m2!1sen!2sin"  height="200" style={{ border: "0",width:"100vw" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+    </footer>
+   </div>
   )
 }
 
--- client/my-project/src/pages/Home.jsx
+++ client/my-project/src/pages/Home.jsx
@@ -1,15 +1,77 @@
-import React from 'react'
-import Footer from '../components/Footer'
+import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link for navigation
+import Footer from '../components/Footer';
+import SimpleImageSlider from 'react-simple-image-slider'
+ import img2 from '../../img2.png';
+ import img3 from '../../img3.png';
+ import img4 from '../../img4.png';
+
 
 function Home() {
+  const sliderImages= [img2,img3,img4];
   return (
-    <div>
-        <div >
-            <header className='text-center text-6xl'>Library</header>
-            <footer className=''><Footer/></footer>
-        </div>
+    <div className='bg-khaki '>
+      <div className='min-h-screen' >
+        {/* Header */}
+        <header className='flex justify-between items-center p-2 bg-navy text-orange-600 font-bold'>
+          <h1 className='sm:text-6xl'>Daksh library</h1>
+          {/* Login Button */}
+          <Link to="/login" className='text-xl hover:underline'>
+            Login
+          </Link>
+        </header>
+
+        {/* slider */}
+         <div className='sm:h-3/4 w-full hidden sm:flex justify-center'>
+
+           <SimpleImageSlider
+            width={"80vw"}
+            height={"100vh"}
+            slideDuration={0.5}
+            images={sliderImages}
+            showNavs={true}
+            loop={true}
+            autoPlay={true}
+         />
+          </div>
+          {/* image for mobile view */}
+          <div className='flex sm:hidden'>
+              <img src={img2} alt="Library image" />
+          </div>
+
+          {/* Facilities */}
+  
+            <div className='my-5'>
+              <header className='font-bold text-5xl font-Montserrat text-center mb-2'>Facilities</header>
+            {/* <div className='w-full sm:h-80 flex flex-col items-center  bg-pink-400 sm:mx-auto px-2 sm:p-0'>
+                <ul className='list-disc flex flex-col justify-evenly  text-2xl text-gray-200 h-full'>
+                  <li>Free WIFI Internet</li>
+                  <li>Fully Air Conditioned</li>
+                  <li>Seperate Sitting for Boys and Girls</li>
+                  <li>Peaceful Enviornment</li>
+                  <li>CCTV Camera Survillence</li>
+                  <li>24 x 7</li>
+                </ul>
+            </div> */}
+            <ul className='grid grid-cols-1 sm:grid-cols-3  list-none text-white font-bold sm:gap-2 sm:mx-1'>
+                 <li className='bg-blue-300 p-3 sm:p-10 text-center'>Free WIFI Internet</li>
+                  <li className='bg-pink-400 p-3 sm:p-10 text-center'>Fully Air Conditioned</li>
+                  <li className='bg-red-300 p-3 sm:p-10 text-center'>Seperate Sitting for Boys and Girls</li>
+                  <li className='bg-green-300 p-3 sm:p-10 text-center'>Peaceful Enviornment</li>
+                  <li className='bg-navy p-3 sm:p-10 text-center'>CCTV Camera Survillence</li>
+                  <li className='bg-yellow-300 p-3 sm:p-10 text-center'>24 x 7</li>
+            </ul>
+            </div>
+
+
+        {/* Footer */}
+        <header className='font-bold text-5xl font-Montserrat text-center my-3'>Details</header>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
