4a5b921b00ca6d6f9eaffe7d587b8b99e6067f4c
1234bhaskar <bhaskar2021chand@gmail.com>
2024-01-03 15:48:44 +0530

    updated customer card component

--- client/my-project/src/components/CustomerCard.jsx
+++ client/my-project/src/components/CustomerCard.jsx
@@ -2,19 +2,11 @@ import React from 'react'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { Navigate, useNavigate } from 'react-router-dom';
 
-function CustomerCard({name,phone,feeStatus,id}) {
+function CustomerCard({name,phone,feeStatus,id,feePaid,totalFee}) {
     const navigate=useNavigate();
-    //  function customFunc(feeStatus) {
-    //     if(feeStatus){
-    //         className={'bg-green-400'}
-    //     }else{
-    //         className={'bg-red-400'}
-
-    //     }
-    // }
   return (
     <div>
-        <div onClick={()=>(navigate("/identity/"+id))} className={`${feeStatus?'bg-green-300':'bg-red-400'} flex  py-2 rounded-sm sm:mx-2 sm:my-2 cursor-pointer my-1 mx-1`}>
+        <div onClick={()=>(navigate("/identity/"+id))} className={`${feePaid===totalFee?'bg-green-300':'bg-red-400'} flex  py-2 rounded-sm sm:mx-2 sm:my-2 cursor-pointer my-1 mx-1`}>
             <AccountCircleIcon className='flex-1' />
             <span className='flex-1'>{name}</span>
             <span className='flex-1'>{phone}</span>
